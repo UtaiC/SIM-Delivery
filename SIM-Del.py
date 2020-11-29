e7a06b986a3de6ad2397750cd75d042f2cbecd2e
Utai <74049768+UtaiC@users.noreply.github.com>
2020-11-29 18:28:03 +0700

    Change Design

--- SIM-Del.py
+++ SIM-Del.py
@@ -4,76 +4,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import altair as alt
-
-#st.set_page_config(layout="wide")
-st.set_page_config(layout="wide")
+############# LOGO #############
 Logo=Image.open('SIM-Logo.jpeg')
 st.image(Logo,width=500)
+#######Heading Text##########
+st.title('**Sales Report**')
+######Delivery Data #1 Data#############
+sales_or=pd.read_excel('Del-Data-or.xlsx')
+sales_or.set_index('Part_No',inplace=True)
+sales1=sales_or.groupby('Part_No').sum()
+######Database Data#####################
+db=pd.read_excel('Database.xlsx')
+db.set_index('Part_No',inplace=True)
+######Delivery-EDI Data Split PartNo and Made New DF #####################
+Del=pd.read_excel('Deli.xlsx')
+Del1= pd.DataFrame(Del['Part'].str.split('/',1).to_list(), columns=["Part_No","Part_Name"])
+Deliver=pd.concat([Del,Del1],axis=1,sort=False)
+Deliver2=Deliver.groupby('Part_No').sum()
+###### Maerge 3 DFs #####################
+sales=pd.merge(Deliver2,db['Price'],on='Part_No',how='left')
+sales2=pd.merge(sales1['TT-B'],sales,on='Part_No',how='left')
+sales2['TT-B-2']=(sales2['Del-Pcs']*sales2['Price'])
+G_Total_Sales=(sales2['TT-B-2'].sum()+(sales2['TT-B'].sum()))
+###### Make Calulate on Deli Data #####################
+sales['Del-Sales']=sales['Del-Pcs']*sales['Price']
+sales['EDI-Sales']=sales['EDI-Pcs']*sales['Price']
+sales['Sales-Pct']=(sales['Del-Sales']/sales['EDI-Sales'])*100
+Pct=sales['Sales-Pct'].mean()
+###### Show Data Details #####################
+Deli_Sales=sales[['EDI-Sales','Del-Sales']]
+Deli_Sales2=sales[['EDI-Sales','Del-Sales','Sales-Pct']]
+Deli_Sales1=Deli_Sales.fillna(0)
+Deli_Sales1=Deli_Sales[Deli_Sales>0]
+Deli_Sales2=Deli_Sales2.dropna()
 
-st.header('**Delivery and Sales Update**')
-st.text('November 2020')
-Del=pd.read_excel('Del-Data.xlsx')
+###### Show Data #####################
+############CheckBox Menu#########
+if st.checkbox('Dailivery and EDI Report'):
+    st.subheader('Daily Delivery details')
+    Deliver[['Part_No','Date','EDI-Pcs','Del-Pcs']]
+if st.checkbox('Sales Report'):
+    st.subheader('Sales Report')
+    Deli_Sales2
+########### Fic On Show % ############
+st.subheader('EDI Vs Delivery (%)')
+st.success(Pct)
+########### Fic On Show B ############
+st.subheader('SUM of Sales (B)')
+sumsales=G_Total_Sales.sum()
+st.success(sumsales)
+if st.checkbox('Sales & EDI on Chart'):
+    st.bar_chart(Deli_Sales1)
+################ Selet Part Section ##############
+sales=pd.merge(db,Deliver2,on='Part_No',how='left')
+sales2=pd.merge(sales1['TT-B'],sales,on='Part_No',how='left')
+sales2.set_index('PartNo',inplace=True)
+sales2['TT-B-2']=(sales2['Del-Pcs']*sales2['Price'])
+sales2['Pct']=(sales2['Del-Pcs']/sales2['EDI-Pcs'])*100
 
-db=pd.read_excel('Database.xlsx','DB')
-
-Delm=pd.merge(db,Del[['Date','Part_No','Qty','TT-B']],on='Part_No',how='left')
-Delm.set_index('PartNo',inplace=True)
-Delm=Delm.fillna(0)
-
-
-
-Sales=Delm[['Date','Qty','TT-B']]
-df = pd.DataFrame(Sales)
-Sales
-#st.dataframe(Sales.style)
-
-
-
-Saleschart=Sales.groupby('PartNo').sum()
-Saleschart=Saleschart[Saleschart>0]
-Saleschart=Saleschart.dropna()
-st.subheader('Sales (B) by Part Update')
-Saleschart['TT-B']
-st.bar_chart(Saleschart['TT-B'])
-st.subheader('Sales (Pcs) by Part Update')
-Saleschart['Qty']
-st.bar_chart(Saleschart['Qty'])
-
-SaleQty=Sales['Qty'].sum()
-Salesupdate=Sales['TT-B'].sum()
-st.subheader('Total Sales Update (B)')
-st.success(Salesupdate)
-st.subheader('Total Sales Update (Pcs)')
-st.success(SaleQty)
-#st.dataframe(Sales.apply(lambda y: "%.2f" % y))
-
-selected_Part = st.sidebar.multiselect('Select PartNo', ['1632','1732','2532','2633','9231','9330','1231','1530','1630','2731','2831','4333','4433','5130','5230','5330','2001','2031','2902','3102','5402',
+###################Select Part Func #################
+selected_Part = st.multiselect('Select PartNo', ['1632','1732','2532','2633','9231','9330','1231','1530','1630','2731','2831','4333','4433','5130','5230','5330','2001','2031','2902','3102','5402',
 '6803','7702','7802','9701','0201','0231','0802','2130','2200','4600','2102','3000','3100','4900','5000','9907','9910','493C','4946','8549','8551','9112','9115','9524',
-'9115','9706','9708','0626','0628','5679','5400','5501','0702','0801','8551','0802','1771','T3100','3113','9775','9680'],default='3000',)
-Show_Sales=Sales.loc[selected_Part]
-
-st.subheader('Sort Sales by PartNo-Selected')
-st.write(Show_Sales)
-SumSales=Show_Sales['Qty'].sum()
-SumSales2=Show_Sales['TT-B'].sum()
-st.subheader('Total (Pcs)')
-st.success(SumSales)
-st.subheader('Total (B)')
-st.success(SumSales2)
-
-Delm.set_index('Type',inplace=True)
-
-selected_Type = st.sidebar.multiselect('Select Type', ['RM-OES','RM-OEM','MC-OES','MC-OEM'],default='RM-OES',)
-Show_Type=Delm.loc[selected_Type]
-
-st.subheader('Sort Sales Update by Type')
-Show_Type[['Date','Part_No','Qty','TT-B']]
-
-SUMType=Show_Type[['Date','Part_No','Qty','TT-B']].sum()['TT-B']
-st.subheader('SUM By Type Total (B)')
-st.success(SUMType)
-
-SUMType2=Show_Type[['Date','Part_No','Qty','TT-B']].sum()['Qty']
-st.subheader('SUM By Type Total (Pcs)')
-st.success(SUMType2)
-
+'9115','9706','9708','0626','0628','5679','5400','5501','0702','0801','8551','0802','1771','T3100','3113','9775','9680'],default=['3000','3100'],)
+Show_Part=sales2.loc[selected_Part][['EDI-Pcs','Del-Pcs','TT-B-2','Pct']]
+st.subheader('Sort Part by Selected')
+st.write(Show_Part)
+############### End ############
+st.warning('End Report')
+###############################
