54734a6bad2a1e50528d7e0134d1aed302a2ec9e
Utai <74049768+UtaiC@users.noreply.github.com>
2020-12-02 10:09:44 +0700

    Adj-Dec

--- SIM-Del.py
+++ SIM-Del.py
@@ -22,10 +22,11 @@ Del1= pd.DataFrame(Del['Part'].str.split('/',1).to_list(), columns=["Part_No","P
 Deliver=pd.concat([Del,Del1],axis=1,sort=False)
 Deliver2=Deliver.groupby('Part_No').sum()
 ###### Maerge 3 DFs #####################
-sales=pd.merge(Deliver2,db['Price'],on='Part_No',how='left')
-sales2=pd.merge(sales1['TT-B'],sales,on='Part_No',how='left')
-sales2['TT-B-2']=(sales2['Del-Pcs']*sales2['Price'])
-G_Total_Sales=(sales2['TT-B-2'].sum()+(sales2['TT-B'].sum()))
+sales=pd.merge(db['Price'],Deliver2,on='Part_No',how='left')
+#sales2=pd.merge(sales1['TT-B'],sales,on='Part_No',how='left')
+sales['TT-B-2']=(sales['Del-Pcs']*sales['Price'])
+G_Total_Sales=(sales['TT-B-2'].sum())
+
 ###### Make Calulate on Deli Data #####################
 sales['Del-Sales']=sales['Del-Pcs']*sales['Price']
 sales['EDI-Sales']=sales['EDI-Pcs']*sales['Price']
@@ -58,15 +59,15 @@ if st.checkbox('Sales & EDI on Chart'):
 ################ Selet Part Section ##############
 sales=pd.merge(db,Deliver2,on='Part_No',how='left')
 sales2=pd.merge(sales1['TT-B'],sales,on='Part_No',how='left')
-sales2.set_index('PartNo',inplace=True)
-sales2['TT-B-2']=(sales2['Del-Pcs']*sales2['Price'])
-sales2['Pct']=(sales2['Del-Pcs']/sales2['EDI-Pcs'])*100
+sales.set_index('PartNo',inplace=True)
+sales['TT-B-2']=(sales['Del-Pcs']*sales['Price'])
+sales['Pct']=(sales['Del-Pcs']/sales['EDI-Pcs'])*100
 
 ###################Select Part Func #################
 selected_Part = st.multiselect('Select PartNo', ['1632','1732','2532','2633','9231','9330','1231','1530','1630','2731','2831','4333','4433','5130','5230','5330','2001','2031','2902','3102','5402',
 '6803','7702','7802','9701','0201','0231','0802','2130','2200','4600','2102','3000','3100','4900','5000','9907','9910','493C','4946','8549','8551','9112','9115','9524',
 '9115','9706','9708','0626','0628','5679','5400','5501','0702','0801','8551','0802','1771','T3100','3113','9775','9680','2300-0','2300-1','2300-2'],default=['3000','3100'],)
-Show_Part=sales2.loc[selected_Part][['EDI-Pcs','Del-Pcs','TT-B-2','Pct']]
+Show_Part=sales.loc[selected_Part][['EDI-Pcs','Del-Pcs','TT-B-2','Pct']]
 st.subheader('Sort Part by Selected')
 st.write(Show_Part)
 ############### End ############
